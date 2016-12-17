feafd728bd9308468745fc001212bfb98149ef03
KRVPerera <rukshan.viduranga@gmail.com>
2016-12-18 00:08:55 +0530

    OpenMP version added
    Signed-off-by: KRVPerera <rukshan.viduranga@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
-#include "A1Config.h"
+
 #include <iostream>
 #include <getopt.h>
 #include <unistd.h>
 #include <random>
+#include <algorithm>
 #include "Util.h"
+#include <omp.h>
 
 using namespace std;
-
-// vector size
-#ifndef N
-#define N 1000
-#endif
 
 
 int main(int argc, char **argv) {
@@ -19,11 +16,13 @@ int main(int argc, char **argv) {
     int c, num_threads;
     struct timespec t0, t1;
     float comp_time;
-    unsigned long 		sec, nsec;
+    unsigned long sec, nsec;
+    int N = 1000;
     opterr = 1;
     seq_ver = p_ver = cuda_ver = veri_run = false;
 
-    while ((c = getopt(argc, argv, "scp:v")) != -1) {
+
+    while ((c = getopt(argc, argv, "scp:vn:")) != -1) {
         switch (c) {
             case 'p':
                 p_ver = true;
@@ -32,6 +31,14 @@ int main(int argc, char **argv) {
                 } catch (std::logic_error) {
                     cerr << "Invalid value for -p, set to 8" << endl;
                     num_threads = 8;
+                }
+                break;
+            case 'n':
+                try {
+                    N = stoi(optarg);
+                } catch (std::logic_error) {
+                    cerr << "Invalid value for -n, set to 1000" << endl;
+                    N = 1000;
                 }
                 break;
             case 's':
@@ -58,7 +65,9 @@ int main(int argc, char **argv) {
 
 #ifdef USE_DOUBLE
     double answer = 0;
-    double answer_s = 0;
+    double answer_c = 0;
+    double answer_p = 0;
+    vector<double> local_sum(num_threads);
     cout << "Generating double vectors " << endl;
     vector<double> vector1(N);
     vector<double> vector2(N);
@@ -70,10 +79,12 @@ int main(int argc, char **argv) {
     }
 #else
     float answer = 0;
-    float answer_s = 0;
+    float answer_c = 0;
+    float answer_p = 0;
     cout << "Generating float vectors " << endl;
     vector<float> vector1(N);
     vector<float> vector2(N);
+    vector<float> local_sum(num_threads);
     for (int j = 0; j < N; ++j) {
         float val = random();
         vector1[j] = val;
@@ -90,35 +101,64 @@ int main(int argc, char **argv) {
     if (p_ver) {
         cout << "Parallel Version running" << endl;
         cout << "number of threads : " << num_threads << endl;
-        pthread_t threadpool[num_threads];
-        answer_s = 0;
+        double start_time, run_time;
+
+        start_time = omp_get_wtime();
+        #pragma omp parallel
+        {
+            double x;
+            int i, id = omp_get_thread_num();
+            num_threads = omp_get_num_threads();
+            int istart = (id * N) / num_threads;
+            int iend = ((id + 1) * N) / num_threads;
+
+            local_sum[id] = 0;
+            for (i = istart; i < iend; i++) {
+                local_sum[id] += vector1[i] * vector2[i];
+            }
+        }
+
+#ifdef USE_DOUBLE
+        for(double n : local_sum) {
+            answer_p += n;
+        }
+#else
+        for (float n : local_sum) {
+            answer_p += n;
+        }
+#endif
+        run_time = omp_get_wtime() - start_time;
+        cout << "Parallel Version Elapsed-time(ms) = " << run_time << " ms" << endl;
     }
 
     if (cuda_ver) {
         cout << "Cuda version is running" << endl;
-        answer_s = 0;
+        answer_c = 0;
     }
 
     if (seq_ver || veri_run) {
-        cout << "Sequential Version running" << endl;
-        GET_TIME(t0);
+        cout << "Sequential Version running" << endl;GET_TIME(t0);
         for (int i = 0; i < N; ++i) {
             answer += vector1[i] * vector2[i];
-        }
-        GET_TIME(t1);
+        }GET_TIME(t1);
         comp_time = Util::elapsed_time_msec(&t0, &t1, &sec, &nsec);
         cout << "Sequential Version Elapsed-time(ms) = " << comp_time << " ms" << endl;
     }
 
 
-    if(veri_run){
-        if(fabs(answer-answer_s) > 0.01){
-            cerr << "Values are different" << endl;
+    if (veri_run) {
+        if (cuda_ver) {
+            if (fabs(answer - answer_c) > 0.01) {
+                cerr << "Values are different" << endl;
+            }
+        } else if (p_ver) {
+            if (fabs(answer - answer_p) > 0.01) {
+                cerr << "Values are different" << endl;
+            }
         }
     }
 
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Successful!" << std::endl;
     return 0;
-
 
 }
