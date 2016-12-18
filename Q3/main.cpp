196953ca356748a0b7753936f9975a5ec0e5af10
KRVPerera <rukshan.viduranga@gmail.com>
2016-12-18 17:49:01 +0530

    Creating Multiple Projects for Q2 and Q3
    Signed-off-by: KRVPerera <rukshan.viduranga@gmail.com>

---
+++ Q3/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by krv on 12/16/16.
+//
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    ios_base::sync_with_stdio(0);
+    cout << "Hello from Q3" << endl;
+}
