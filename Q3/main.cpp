7829f066341db47b677a0fbf60e258f2a19f4ffd
KRVPerera <rukshan.viduranga@gmail.com>
2016-12-25 17:21:05 +0530

    Warnings fixed

--- Q3/main.cpp
+++ Q3/main.cpp
@@ -72,29 +72,30 @@ matrixMultiKernel(double *C, double *A, double *B, int Width){
 
 	double temp_c = 0;
 
-		// sub matrices
-		__shared__ double sub_a[BLOCK_SIZE][BLOCK_SIZE];
-		__shared__ double sub_b[BLOCK_SIZE][BLOCK_SIZE];
+
 	// loop throught the submatrices
 	for(int a = a_begin, b = b_begin; a <= a_end; 
-			a += a_step, b += b_step)
-	{
-		
-		sub_a[ty][tx] = A[ a + Width * ty + tx];
-		sub_b[ty][tx] = A[ b + Width * ty + tx];
+			a += a_step, b += b_step) {
+        // sub matrices
+        __shared__ double sub_a[BLOCK_SIZE][BLOCK_SIZE];
+        __shared__ double sub_b[BLOCK_SIZE][BLOCK_SIZE];
 
-		__syncthreads();
-	}
+        sub_a[ty][tx] = A[a + Width * ty + tx];
+        sub_b[ty][tx] = A[b + Width * ty + tx];
 
-	// loop unroll may not work on cuda if compilation level -O3
-	// effects cuda code as wll in the assignment
-	// sub matrix multiplication
-	#pragma unroll
-	for(int k = 0; k < BLOCK_SIZE; ++k){
-		temp_c += sub_a[ty][k] * sub_b[k][tx];
-	}
-	// sync all the global threads running the computations
-	__syncthreads();
+        __syncthreads();
+
+
+        // loop unroll may not work on cuda if compilation level -O3
+        // effects cuda code as wll in the assignment
+        // sub matrix multiplication
+#pragma unroll
+        for (int k = 0; k < BLOCK_SIZE; ++k) {
+            temp_c += sub_a[ty][k] * sub_b[k][tx];
+        }
+        // sync all the global threads running the computations
+        __syncthreads();
+    }
 	int c = Width * BLOCK_SIZE * by + BLOCK_SIZE * bx;
 	C[c + Width * ty + tx ] = temp_c;
 }
@@ -230,7 +231,7 @@ int main(int argc, char **argv) {
     double *mat_c_ans = new double [N*N];
 
     // cuda device pinters
-    double *d_mat1,d_mat2, d_mat_c_ans;
+    double *d_mat1,*d_mat2, *d_mat_c_ans;
 
 
 #pragma omp parallel
@@ -260,7 +261,7 @@ int main(int argc, char **argv) {
     float *mat_p_ans = new float [N*N];
     float *mat_c_ans = new float [N*N];
      // cuda device pinters
-    float *d_mat1,d_mat2, d_mat_c_ans;
+    float *d_mat1, *d_mat2, *d_mat_c_ans;
      omp_set_num_threads(num_threads);
 #pragma omp parallel
     {
@@ -324,8 +325,8 @@ int main(int argc, char **argv) {
         HANDLE_ERROR(cudaMalloc((void **) &d_mat2, N * N *  sizeof(double)));
         HANDLE_ERROR(cudaMalloc((void **) &d_mat_c_ans, N * N * sizeof(double)));
         // copy host memory to device
-        HANDLE_ERROR(cudaMemcpy((void **)d_mat1,(void **) mat1, N * N * sizeof(double), cudaMemcpyHostToDevice));
-        HANDLE_ERROR(cudaMemcpy((void **)d_mat2,(void **) mat2, N * N * sizeof(double), cudaMemcpyHostToDevice));
+        HANDLE_ERROR(cudaMemcpy(d_mat1, mat1, N * N * sizeof(double), cudaMemcpyHostToDevice));
+        HANDLE_ERROR(cudaMemcpy(d_mat2, mat2, N * N * sizeof(double), cudaMemcpyHostToDevice));
         matrixMultiKernel<<< grid, threads >>>(d_mat_c_ans, d_mat1, d_mat2, N);
         HANDLE_ERROR(cudaMemcpy(d_mat_c_ans, d_mat_c_ans, N * N * sizeof(double), cudaMemcpyDeviceToHost));
         GET_TIME(t1);
